435957268c1351f57ff7f47c5f32ab8eabe229f1
Radu Nistor <radunstr@yahoo.com>
2023-09-28 09:30:29 +0300

    added comment

--- index.php
+++ index.php
@@ -9,7 +9,8 @@ set_exception_handler("ErrorHandler::handleException");
 
 header("Content-type: application/json; charset=UTF-8");
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
-if ($parts[2] != "words") {
+// the array index depends on project URL
+if ($parts[1] != "words") {
     echo 'Not found';
     http_response_code(404);
     exit;
